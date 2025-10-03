48849eca148d732591211b8cd2c56af9baa27b82
mmoya26 <mmoya18@icloud.com>
2025-10-03 13:18:44 -0500

    add close button + update shipping address template to add states

--- src/app/components/print-services/shipping-address/shipping-address.ts
+++ src/app/components/print-services/shipping-address/shipping-address.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './shipping-address.css'
 })
 export class ShippingAddress {
-
+  US_STATES: readonly string[] = [
+    "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+    "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+    "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+    "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+    "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+  ];
 }
