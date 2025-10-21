5588f11baca00fe23e16dbf4f21c355b36eb1130
Miguel Moya <mmoya18@icloud.com>
2025-10-21 14:42:41 -0500

    add count of services activated

--- src/app/components/list-of-print-services/list-of-print-services.ts
+++ src/app/components/list-of-print-services/list-of-print-services.ts
@@ -1,23 +1,45 @@
-import { Component, inject, OnInit, output } from '@angular/core';
+import { Component, computed, inject, OnInit, output, signal } from '@angular/core';
 import { Service, ServiceCategory, ServiceType } from '../../app';
 import { PrintServices } from '../../services/print-services';
+import { RecipientList } from '../print-services/recipient-list/recipient-list';
 
 @Component({
   selector: 'app-list-of-print-services',
   imports: [],
   templateUrl: './list-of-print-services.html',
-  styleUrl: './list-of-print-services.css'
+  styleUrl: './list-of-print-services.css',
 })
 export class ListOfPrintServices implements OnInit {
   printService = inject(PrintServices);
   editServiceEvent = output<Service>();
 
-  premiumServices: Service[] = [];
-  standardServices: Service[] = [];
+  premiumServices = signal<Service[]>([]);
+  standardServices = signal<Service[]>([]);
+
+  activePremiumServicesCount = computed(() => {
+    const length = this.premiumServices().filter((s) =>
+      this.printService.activeServices$().find((ac) => ac === s.key),
+    ).length;
+
+    return length;
+  });
+
+  activeStandardServicesCount = computed(() => {
+    const length = this.standardServices().filter((s) =>
+      this.printService.activeServices$().find((ac) => ac === s.key),
+    ).length;
+
+    return length;
+  });
 
   ngOnInit() {
-    this.premiumServices = this.printService.services.filter(s => s.category === ServiceCategory.Premium);
-    this.standardServices = this.printService.services.filter(s => s.category === ServiceCategory.Standard);
+    this.premiumServices.set(
+      this.printService.services.filter((s) => s.category === ServiceCategory.Premium),
+    );
+
+    this.standardServices.set(
+      this.printService.services.filter((s) => s.category === ServiceCategory.Standard),
+    );
   }
 
   editService(service: Service) {
